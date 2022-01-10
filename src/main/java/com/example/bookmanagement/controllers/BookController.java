c65d6ae789b8cd849bdd9fe8896b87d7c57beb77
morris-wambua <morriswambua7@gmail.com>
2022-01-10 15:22:09 +0300

    adding book Controller

---
+++ src/main/java/com/example/bookmanagement/controllers/BookController.java
@@ -0,0 +1,26 @@
+package com.example.bookmanagement.controllers;
+
+import com.example.bookmanagement.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    //Inject book repository
+    private final BookRepository bookRepository;
+
+    //create a constructor
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
